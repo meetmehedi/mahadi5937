32068a3467673dbb11a8e240d388b9c3bd3bdb81
MD. Mehedi Hasan <167318121+meetmehedi@users.noreply.github.com>
2024-11-05 21:29:00 +0600

    Bracket match

--- main.c
+++ main.c
@@ -1,69 +1,80 @@
-//
-//  main.c
-//  insertion
-//
-//  Created by Md. Mehedi Hasan on 21/4/24.
-//
 #include <stdio.h>
-#include <stdlib.h>
-
-struct node
+#define n 10
+char stack[n];
+int top = -1;
+ 
+int isFull(void)
 {
-    int data;
-    struct node *next;
-};
-
-int main(void)
+    if(top==n-1)
+        return 1;
+    else
+        return 0;
+ 
+}
+ 
+int isEmpty(void)
 {
-    int n,i, ins, value;
-    struct node *current = NULL, *previous = NULL, *head = NULL, *h1, *newnode;
-    printf("Enter the number of nodes: ");
-    scanf("%d",&n);
-
-    for (i=0;i<n;i++)
+    if(top==-1)
+        return 1;
+    else
+        return 0;
+ 
+}
+ 
+char pop(void)
+{
+    char val;
+    if (isEmpty())
     {
-        current = malloc(sizeof(struct node));
-        scanf("%d",&current->data);
-        if(i==0)
-            head = current;
-        else
-            previous->next = current;
-
-        previous = current;
-    }
-    current->next = NULL;
-    h1 = head;
-    while(h1 != NULL)
-    {
-        printf("%d ", h1->data);
-        h1 = h1->next;
-    }
-    printf("\nEnter the position: ");
-    scanf("%d",&ins);
-    printf("Enter the value: ");
-    scanf("%d",&value);
-    newnode = malloc(sizeof(struct node));
-    newnode->data = value;
-
-    h1 = head;
-    if (ins == 1)
-    {
-        newnode->next = head;
-        head = newnode;
+        printf("Stack is Empty\n");
+        return '\0';
     }
     else
     {
-        for(i=1;i<=ins-2;i++)
-        {
-            h1 = h1->next;
-        }
-        newnode->next = h1->next;
-        h1->next = newnode;
-    }
-    h1 = head;
-    while(h1 != NULL)
-    {
-        printf("%d ", h1->data);
-        h1 = h1->next;
+        val = stack[top];
+        top--;
+        return val;
     }
 }
+
+void push(char val)
+{
+    if (isFull())
+        printf("Stack is Full\n");
+    else
+    {
+        top++;
+        stack[top]=val;
+    }
+}
+ 
+int main(void)
+ 
+{
+    char x[50],f;
+    int i;
+    scanf("%s",x);
+    for(i=0;x[i]!='\0';i++)
+    {
+        if(x[i]=='['||x[i]=='{'||x[i]=='(')
+            push(x[i]);
+        else if (x[i]==']'||x[i]=='}'||x[i]==')')
+        {
+            f = pop();
+            if((f=='['&&x[i]==']')||(f=='{'&&x[i]=='}')||(f=='('&&x[i]==')'))
+                continue;
+            else
+            {
+                printf("Unbalanced\n");
+                return 0;
+            }
+        }
+        else
+            continue;
+    }
+    if (isEmpty())
+        printf("Balanced ");
+    else
+        printf("Unbalanced ");
+}
+ 
